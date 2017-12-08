a5dcd4dd779e3e8267fdf549b4e25a19252f39fe
Dan Nollette <nollette.dan@gmail.com>
2017-12-08 15:18:56 -0800

    add product detail show/hide capability.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price)

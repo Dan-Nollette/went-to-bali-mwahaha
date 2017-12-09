ea6d0c462607e7ca1404d5985c9314363a2b84ca
Dan Nollette <nollette.dan@gmail.com>
2017-12-08 16:22:13 -0800

    Add product validations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   has_many :order_items
+  validates :name, :presence => true
+  validates :description, :presence => true
+  validates :price, :presence => true
 end
